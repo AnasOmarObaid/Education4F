3acb3a973f846ded724b81d67e8c6a7d651c4f63
AnasOmarObaid <ansmr002@gmail.com>
2022-03-12 01:08:56 +0200

    working on series page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,28 +1,35 @@
+jQuery(document).ready(function ($) {
 
-
-$(document).ready(function(){
-   
+    "use strict";
 
     // owl setup
-
     $('.owl-carousel').owlCarousel({
         center: true,
-        loop:true,
-        margin:30,
-        nav:false,
-        autoplay:true,
-        autoplayTimeout:4000,
-        autoplayHoverPause:true,
-        responsive:{
-            0:{
-                items:2
+        loop: true,
+        margin: 30,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2
             },
-            600:{
-                items:3
+            600: {
+                items: 3
             },
-            1000:{
-                items:6
+            1000: {
+                items: 6
             }
         }
     });
+
+    // NProgress
+    $('body').show();
+	NProgress.start();
 });
+
+$(window).on('load', function () {
+    setTimeout(function() { NProgress.done()}, 2000);
+});
+ 
